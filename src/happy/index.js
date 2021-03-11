87148b22eb76798e6cef33fdc03dfc7b32ecab65
ellartdev <ott.ellart99@gmail.com>
2021-03-11 20:09:07 +0200

    happy pictrues
    with index.js lmao

---
+++ src/happy/index.js
@@ -0,0 +1,10 @@
+export * as happy1 from './happy1.jpg';
+export * as happy2 from './happy2.jpg';
+export * as happy3 from './happy3.jpg';
+export * as happy4 from './happy4.jpg';
+export * as happy5 from './happy5.jpg';
+
+/**
+ * this is some funky stuff
+ * let me tell you!
+ */
